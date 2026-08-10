b5f4db26cd89a5e8d03ff57db35755fb846f6af5
Ramon Adàlia <adaliaramon@gmail.com>
2026-08-10 09:12:38 +0200

    Added better detection of CPU usage

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER " "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,7 +18,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "cat /sys/class/power_supply/BAT1/capacity | xargs -I {} echo {}%", 5, 9) \
-    X("", "date '+%a %d/%m/%y %H:%M:%S'", 1, 10)
+    X("CPU: ", "{ awk '/^cpu / {t=0; for(i=2;i<=NF;i++) t+=$i; print t,$5+$6; exit}' /proc/stat; sleep 1; awk '/^cpu / {t=0; for(i=2;i<=NF;i++) t+=$i; print t,$5+$6; exit}' /proc/stat; } | awk 'NR==1 {t=$1; i=$2; next} {printf \"%.2f%%\", 100*(1-($2-i)/($1-t))}'", 1, 9) \
+    X("RAM: ", "free --mebi | awk '/^Mem:/ {printf \"%.2fGiB\", $3/1024}'", 1, 11) \
+    X("", "date '+%d/%m/%y %H:%M:%S'", 1, 10)
 
 #endif  // CONFIG_H
